d1030298ccc00735f0e527e910b5d30abe58e621
Raminder Singh <raminder@AL7AF396.its.caregroup.org>
2017-09-07 11:55:47 -0400

    updated docs

--- docs/conf.py
+++ docs/conf.py
@@ -30,7 +30,7 @@ needs_sphinx = '1.5.3'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.imgmath', 'nbsphinx']
+extensions = ['sphinx.ext.imgmath']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
